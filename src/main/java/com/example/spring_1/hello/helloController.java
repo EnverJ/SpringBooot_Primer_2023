458c64e9c299a9fd711936ddfe992e3d445aa996
EnverJ <Enlem1987@gmail.com>
2023-05-22 22:13:13 -0600

    h2

--- src/main/java/com/example/spring_1/hello/helloController.java
+++ src/main/java/com/example/spring_1/hello/helloController.java
@@ -1,5 +1,6 @@
 package com.example.spring_1.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class helloController {
+  @Autowired
+  private HelloService service;
 
   @GetMapping("/hello")
   public String getHello() {
@@ -19,6 +22,17 @@ public class helloController {
   public String postRequest(@RequestParam("text1") String str, Model model) {
     // Register the String received from the screen to Model
     model.addAttribute("sample", str);
-    return "/hello/response";
+    return "hello/response";
+  }
+
+  @PostMapping("/hello/db")
+  public String postDbRequest(@RequestParam("text2") String id, Model model) {
+    //search one
+    Employee employee = service.getEmployee(id);
+
+    //save Search Result to Model
+    model.addAttribute("employee", employee);
+    //screen transition to db.html
+    return "hello/db";
   }
 }
